122ff77a5d4b607d1756e3f0c75536b512937c06
Arthur Henrique Paulini Grasnievicz <98137147+arthurGNZ@users.noreply.github.com>
2024-08-21 21:02:39 -0300

    Update problema_mochila.cpp

--- problema_mochila.cpp
+++ problema_mochila.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 using namespace std;
+
 /*
 ---Problema da mochila---
 Entrada: Quantidade de itens n e capacidade W da mochila para cada item i, valor Vi do item e peso Pi do item
@@ -19,6 +21,22 @@ int forca_bruta(int i, int capacidade, int *pesos, int *valores, int n){
     }
     return max((valores[i]+forca_bruta(i+1, capacidade-pesos[i], pesos, valores, n)),(forca_bruta(i+1,capacidade,pesos,valores, n)));
 }
+
+//nesta solução, os casos são salvos. Assim, não irá gastar processamento com coisas que já foram
+int solucao_2(int i, int capacidade, int *pesos, int *valores, int n, vector<std::vector<int>> &m){
+    if(i>=n || capacidade==0){
+        return 0;
+    }
+    if(m[i][capacidade]!=-1){
+        return m[i][capacidade];
+    }
+    if(pesos[i]>capacidade){
+        m[i][capacidade]=solucao_2(i+1,capacidade, pesos, valores, n,m);
+        return m[i][capacidade];
+    }
+    m[i][capacidade]=max((valores[i]+solucao_2(i+1, capacidade-pesos[i], pesos, valores, n,m)),(solucao_2(i+1,capacidade,pesos,valores, n,m)));
+    return m[i][capacidade];
+}
 int main() {
     int quant;
     int cap;
@@ -34,5 +52,13 @@ int main() {
         cout << "Digite o peso do item: ";
         cin >> pesos[i];
     }
-    cout<<forca_bruta(0,cap,pesos,valores,quant);
+    vector<std::vector<int>> m;
+    m.resize(quant);
+    for(int i=0; i<quant; i++){
+        m[i].resize(cap+1,-1);
+    }
+
+      
+    //cout<<forca_bruta(0,cap,pesos,valores,quant);
+    cout<<solucao_2(0,cap,pesos,valores,quant, m);
 }
